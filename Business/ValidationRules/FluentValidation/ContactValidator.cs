1cc290153f739ef6b658d4a04beffafffd399b33
zeljkosamed <zeljkosamed@users.noreply.github.com>
2021-09-07 14:01:42 +0300

    İletişim kısmına başlandı.

---
+++ Business/ValidationRules/FluentValidation/ContactValidator.cs
@@ -0,0 +1,24 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class ContactValidator : AbstractValidator<Contact>
+    {
+        public ContactValidator()
+        {
+            RuleFor(c => c.UserMail).NotEmpty().WithMessage("Mail alanı boş geçilemez!");
+            RuleFor(c => c.Subject).NotEmpty().WithMessage("Konu alanı boş geçilemez!");
+            RuleFor(c => c.Message).NotEmpty().WithMessage("Mesaj alanı boş geçilemez!");
+            RuleFor(c => c.UserName).NotEmpty().WithMessage("Kullanıcı adı boş geçilemez!");
+            RuleFor(c => c.Subject).MinimumLength(3).WithMessage("Lütfen en az 3 (üç) karakter giriniz!");
+            RuleFor(c => c.UserName).MinimumLength(3).WithMessage("Lütfen en az 3 (üç) karakter giriniz!");
+            RuleFor(c => c.Subject).MaximumLength(50).WithMessage("Lütfen en fazla 50 (elli) karakter giriniz!");
+        }
+    }
+}
---
+++ MvcProjeKampi/Controllers/ContactController.cs
@@ -0,0 +1,23 @@
+﻿using Business.Concrete;
+using Business.ValidationRules.FluentValidation;
+using DataAccess.Concrete.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjeKampi.Controllers
+{
+    public class ContactController : Controller
+    {
+        ContactManager contactManager = new ContactManager(new EfContactDal());
+        ContactValidator contactValidator = new ContactValidator();
+        // GET: Contact
+        public ActionResult Index()
+        {
+            var contactList = contactManager.GetAll();
+            return View(contactList);
+        }
+    }
+}
